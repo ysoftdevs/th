1e24e656e71de5566ca441a1b63f0025a409fbf1
Jakub Fojtl <jakub.fojtl@ysoft.com>
2015-04-11 20:39:48 +0200

    Mark Seemann

---
+++ 2015_02_19_Seemann_Mark_Equivalence_Classes_xunitnet_FsCheck_Property-based_testing/FizzBuzz/FizzBuzz.fs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2014 Mark Seemann.
+// Permission to reproduce or modify is granted for personal, educational use.
+// No warranty implied.
+
+namespace Ploeh.Samples
+
+open Xunit.Extensions
+open FsCheck
+open FsCheck.Xunit
+
+module FizzBuzz =
+    let transform number = 
+        match number % 3, number % 5 with
+        | 0, 0 -> "FizzBuzz"
+        | _, 0 -> "Buzz"
+        | 0, _ -> "Fizz"
+        | _ -> number.ToString()
+
+module Tests =
+    [<Property(QuietOnSuccess = true)>]
+    let ``FizzBuzz.transform returns number`` (number : int) =
+        (number % 3 <> 0 && number % 5 <> 0) ==> lazy
+        let actual = FizzBuzz.transform number
+        let expected = number.ToString()
+        expected = actual
+
+    [<Property(QuietOnSuccess = true)>]
+    let ``FizzBuzz.transform returns Fizz`` (number : int) =
+        (number % 3 = 0 && number % 5 <> 0) ==> lazy
+        let actual = FizzBuzz.transform number
+        let expected = "Fizz"
+        expected = actual
+
+
+    [<Property(QuietOnSuccess = true)>]
+    let ``FizzBuzz.transform returns Buzz`` (number : int) =
+        (number % 5 = 0 && number % 3 <> 0) ==> lazy
+        let actual = FizzBuzz.transform number
+        let expected = "Buzz"
+        expected = actual
+
+    type DivisibleByThreeAndFive =
+        static member Int() =
+            Arb.Default.Int32()
+            |> Arb.mapFilter (fun x -> x * 3 * 5) (fun _ -> true)
+    
+    [<Property(Arbitrary = [| typeof<DivisibleByThreeAndFive> |], QuietOnSuccess = true)>]
+    let ``FizzBuzz.transform returns FizzBuzz`` (number : int) =
+        let actual = FizzBuzz.transform number
+        let expected = "FizzBuzz"
+        expected = actual
---
+++ 2015_02_19_Seemann_Mark_Equivalence_Classes_xunitnet_FsCheck_Property-based_testing/README.md
@@ -0,0 +1,8 @@
+# Equivalence Classes, xUnit.net, FsCheck, Property-Based Testing
+
+Original code can be found at [github.com/ploeh/EquivalenceClassesFsCheckFizzBuzz](https://github.com/ploeh/EquivalenceClassesFsCheckFizzBuzz).
+
+This is essentially the same code that I write in my *Equivalence Classes, xUnit.net, FsCheck, Property-Based Testing* presentation, of which live recordings exist:
+
+- [NDC London 2014](https://vimeo.com/113588391)
+- [YSoft Technology Hour, Prague 2015](https://youtu.be/2oN9caQflJ8)
